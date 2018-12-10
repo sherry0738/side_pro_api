a0f498ce511eb7f3a9c8e85c801fa997d054a259
Sherry Li <shli@zendesk.com>
2018-12-11 02:03:15 +1100

    Validate bearer authorization header by decoding authToken

--- index.js
+++ index.js
@@ -3,9 +3,24 @@ const cors = require('cors')
 const app = express()
 const port = 3001
 
-app.use(cors())
-app.get('/', (req, res) => res.send(
-    {
+function parsetJwtToken(authToken) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse(Buffer.from(base64, 'base64').toString());
+    }
+
+    app.use(cors())
+    app.get('/', (req, res, next) => {
+    var auth = req.get("authorization");
+        // On the first request, the "Authorization" header won't exist, so we'll set a Response
+        // header that prompts the browser to ask for a username and password.
+    if (auth  && auth.split(' ')[0].toLowerCase() === 'bearer') {
+    const decodedToken = parsetJwtToken(auth);
+        // console.log('decoded', decodedToken)
+        // The username and password are correct, so the user is authorized.
+
+      return res.send( 
+      {
         "quizzes": [
             {
               "question_body": "Which color listed below used for Zendesk socks design?",
@@ -53,10 +68,13 @@ app.get('/', (req, res) => res.send(
             }
           ],
           "totalCount": 2
-        }
-    
-    
-      
-       ))
+        });
+    } else {
+        res.set("WWW-Authenticate", "Basic realm=\"Authorization Required\"");
+        // If the user cancels the dialog, or enters the password wrong too many times,
+        // show the Access Restricted error message.
+        return res.status(401).send("Authorization Required");
+    }
+  })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
